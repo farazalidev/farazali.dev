40314c425afcd3ff702672ac95ba338d8afc1b6d
farazalidev <farazalidev@gmail.com>
2024-05-19 14:26:53 +0500

    Fixes: server error when going back to url history

--- apps/web/src/components/Blog/MarkdownHeading.tsx
+++ apps/web/src/components/Blog/MarkdownHeading.tsx
@@ -1,47 +1,20 @@
 'use client';
-import { useEffect, useRef, type HTMLAttributes } from 'react';
+import { useEffect, useState, type HTMLAttributes } from 'react';
 
 interface MarkdownHeadingProps extends HTMLAttributes<HTMLDivElement> {
     elementType: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }
 
-type HeadingElRef = HTMLHeadElement | null;
-
 export const MarkdownHeading: React.FC<MarkdownHeadingProps> = ({ ...props }) => {
-    const sectionsRef = useRef<HeadingElRef[]>([]);
+    const [isClient, setIsClient] = useState(false);
 
     useEffect(() => {
-        const section = sectionsRef.current;
-
-        const options = {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.5,
-        };
-
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    const sectionId = entry.target.id;
-                    history.replaceState(null, '', `#${sectionId}`);
-                }
-            });
-        }, options);
-
-        section.forEach((item) => {
-            observer.observe(item as Element);
-        });
-
-        return () => {
-            section.forEach((item) => {
-                observer.unobserve(item as Element);
-            });
-        };
+        setIsClient(true);
     }, []);
 
-    return (
-        <props.elementType className='prose toc-heading' id={`${props.children as string[][0]}`} ref={(el) => sectionsRef.current.push(el)}>
+    return isClient ? (
+        <props.elementType className='prose toc-heading' id={`${props.children as string[][0]}`}>
             {props.children}
         </props.elementType>
-    );
+    ) : null;
 };
